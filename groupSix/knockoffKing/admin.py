071da544ba397a110c3be460351e0c7e9b32ecb3
Jacob <jra457@msstate.edu>
2023-04-05 11:11:28 -0500

    Updated (Django) admin paige

--- groupSix/knockoffKing/admin.py
+++ groupSix/knockoffKing/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Product, Admin, Customer, Seller, \
+    OrderDetails, ShippingInfo, Order, ShoppingCart, CartItem
+
+admin.site.register(User)
+admin.site.register(Product)
+admin.site.register(Admin)
+admin.site.register(Customer)
+admin.site.register(Seller)
+admin.site.register(OrderDetails)
+admin.site.register(ShippingInfo)
+admin.site.register(Order)
+admin.site.register(ShoppingCart)
+admin.site.register(CartItem)
