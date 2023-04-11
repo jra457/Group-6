6a9c5fb5f416686bd6626b2e200cb7f12b77f086
Stephen Proctor <sproctor291@gmail.com>
2023-04-11 17:32:13 -0500

    Initiated test files, no successful tests run yet

--- groupSix/tests/test_views.py
+++ groupSix/tests/test_views.py
@@ -1,1 +1,9 @@
-from django import TestCase
+from django.test import TestCase
+from django.urls import reverse
+from knockoffKing.models import UserModel, Product, Admin, Customer, Seller, OrderDetails, ShippingInfo, Order, ShoppingCart, CartItem
+
+
+class UserModelViewTest(TestCase):
+
+    def setUp(self):
+
