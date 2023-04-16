0bad861123d186d41c55e22752c871afa9055d2a
Stephen <sproctor291@gmail.com>
2023-04-16 15:08:55 -0500

    changed test files

--- groupSix/tests/test_views.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from django.urls import reverse
-from knockoffKing.models import UserModel, Product, Admin, Customer, Seller, OrderDetails, ShippingInfo, Order, ShoppingCart, CartItem
-
-
-class UserModelViewTest(TestCase):
-
-    def setUp(self):
-
